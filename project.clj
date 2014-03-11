0e35b7e3cd8ced8a0acc1627527bf7b45e6aca74
David Park <daviddpark@gmail.com>
2014-03-11 17:14:45 -0400

    lein-midje

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
                  [clojure-saxon "0.9.3"]]
-  :profiles {:dev {:dependencies [[lein-midje "3.1.3"]
-                                  [midje "1.6.2"]
+  :profiles {:dev {:dependencies [[midje "1.6.2"]
                                   [midje-junit-formatter "0.1.0-SNAPSHOT"]
                                   [org.clojars.runa/conjure "2.2.0"]]
+                   :plugins [[lein-midje "3.1.3"]]
                    :resource-paths ["sample_ccdas/EMERGE"]}}  )
